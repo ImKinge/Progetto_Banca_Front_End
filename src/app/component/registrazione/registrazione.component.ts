06a1596f436e2b9c8a39d9cde08f3dbd26e05841
faiolagianmarco@gmail.com <G!@nm@rc095!$>
2023-03-24 12:58:22 +0100

    Implementazione registrazione.component html e css Implementazione dati-utente.component html e css Navbar spostata nell'app.component html modifiche login.component html css e ts

--- src/app/component/registrazione/registrazione.component.ts
+++ src/app/component/registrazione/registrazione.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ClienteService} from "../../servizi/servizi-cliente/cliente.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registrazione',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./registrazione.component.css']
 })
 export class RegistrazioneComponent {
+  message = "Anagraphic User Data";
 
+  constructor(private router:Router){};
+
+
+  goToDatiUtenti() {
+    this.router.navigate(["dati-utente"]);
+  }
 }
--- src/app/models/anagraficaCliente.ts
+++ src/app/models/anagraficaCliente.ts
@@ -7,4 +7,5 @@ export class AnagraficaCliente {
     public recapitoTelefonico = null;
     public indirizzo = null;
 
+
 }
